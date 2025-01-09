9877ee77560902f8b6887f4fd654b3c6c5ba1271
Wen <150439278+WenW186079@users.noreply.github.com>
2025-01-09 21:28:32 +0100

    Add files via upload

---
+++ DPO_QLORA/dpo_train_with_qlora.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+# example usage
+# sh scripts/finetune_with_accelerate_config.sh 1 configs/train_configs/sft/default.yaml
+# sh scripts/finetune_with_accelerate_config.sh 8 configs/train_configs/sft/olmo_17_sft.yaml
+
+# Check if exactly two arguments are provided
+if [ "$#" -ne 2 ]; then
+    echo "Usage: $0 <num_gpus> <config_file>"
+    echo "Example: $0 2 path/to/config.yaml"
+    exit 1
+fi
+
+NUM_GPUS="$1"
+CONFIG_FILE="$2"
+
+# Generate CUDA_VISIBLE_DEVICES as a range from 0 to NUM_GPUS-1
+CUDA_VISIBLE_DEVICES=$(seq -s, 0 $((NUM_GPUS-1)))
+#CUDA_VISIBLE_DEVICES=0,2,3,4,5,6,7,8
+export CUDA_VISIBLE_DEVICES
+
+echo "Number of GPUs: $NUM_GPUS"
+echo "Using config file: $CONFIG_FILE"
+echo "CUDA_VISIBLE_DEVICES: $CUDA_VISIBLE_DEVICES"
+
+
+# Lora training
+accelerate launch \
+    --num_machines 1 \
+    --num_processes $NUM_GPUS \
+    --use_deepspeed \
+    --deepspeed_config_file configs/ds_configs/stage2.conf \
+    open_instruct/dpo_tune.py \
+    "$2"
+
+# python open_instruct/merge_lora.py \
+#     --base_model_name_or_path WenWW/sparse_Llama_8B_2of4_SFT_qlora_test \
+#     --lora_model_name_or_path output/tulu_v2_dpo_qlora/ \
+#     --output_dir output/tulu_v2_dpo_qlora_merged/ \
+#     --qlora \
+#     --push_to_hub \
+#     --save_tokenizer 
