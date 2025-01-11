82c55114ea7198454f634a8f5cc905e9d83cc997
Wen <150439278+WenW186079@users.noreply.github.com>
2025-01-11 23:27:37 +0100

    Add files via upload

---
+++ qlora/dpo_qlora_merge.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+python open_instruct/merge_lora.py \
+    --lora_model_name_or_path "WenWW/Sparse8B_TO_DPO_Qlora" \
+    --base_model_name_or_path "neuralmagic/Sparse-Llama-3.1-8B-2of4" \
+    --tokenizer_name_or_path "neuralmagic/Sparse-Llama-3.1-8B-2of4" \
+    --output_dir output/dpo_8b_Qlora_merged \
+    --pad_to_multiple_of 8 \
+    --push_to_hub \
+    --save_tokenizer \
+    --hf_repo_id "WenWW/Sparse8B_TO_DPO_Qlora_merged"
+
+
