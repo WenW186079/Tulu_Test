656238c58142646900b827d934a4756fb60f066b
Wen <150439278+WenW186079@users.noreply.github.com>
2025-01-12 03:04:18 +0100

    Update dpo_train_with_qlora.sh

--- qlora/dpo_train_with_qlora.sh
+++ qlora/dpo_train_with_qlora.sh
@@ -31,3 +31,20 @@ accelerate launch \
     --deepspeed_config_file configs/ds_configs/stage2.conf \
     open_instruct/dpo_tune_qlora.py \
     "$2"
+
+python open_instruct/merge_lora.py \
+    --config_file $CONFIG_FILE \
+    --push_to_hub \
+    --save_tokenizer
+
+# python open_instruct/merge_lora.py \
+#     --lora_model_name_or_path "WenWW/Sparse8B_TO_DPO_Qlora" \
+#     --base_model_name_or_path "neuralmagic/Sparse-Llama-3.1-8B-2of4" \
+#     --tokenizer_name_or_path "neuralmagic/Sparse-Llama-3.1-8B-2of4" \
+#     --output_dir output/dpo_8b_Qlora_merged \
+#     --pad_to_multiple_of 8 \
+#     --push_to_hub \
+#     --save_tokenizer \
+#     --hf_repo_id "WenWW/Sparse8B_TO_DPO_Qlora"
+#     #--config_file $CONFIG_FILE \
+
