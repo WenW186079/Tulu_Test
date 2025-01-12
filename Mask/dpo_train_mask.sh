809630f14c1ace240430b655097b73491e53bba2
Wen <150439278+WenW186079@users.noreply.github.com>
2025-01-12 16:58:25 +0100

    Update dpo_train_mask.sh

--- Mask/dpo_train_mask.sh
+++ Mask/dpo_train_mask.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # example usage
-# sh scripts/dpo_train_with_accelerate_config.sh 8 configs/train_configs/dpo/default.yaml
+# sh Mask/dpo_train_mask.sh 2 Mask/8b_dpo_mask.yaml
 
 # Check if exactly two arguments are provided
 if [ "$#" -ne 2 ]; then
